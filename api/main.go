9e9628779f213f58b4a7a95327adb5f4031a2791
川野勇也 <kawanoisamunari@kawanoisamunarinoMacBook-Pro.local>
2023-02-07 18:19:25 +0900

    intial commit （db接続）

---
+++ api/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func dbConnect() *gorm.DB {
+	dsn := "webuser:webpass@tcp(db:3306)/keijiban?charset=utf8mb4"
+	count := 0
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		for {
+			if err == nil {
+				break
+			}
+			fmt.Println("データベース再接続...")
+			time.Sleep(time.Second)
+			count++
+			if count > 60 {
+				fmt.Println("データベース接続失敗")
+				log.Fatal(err)
+			}
+			db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		}
+	}
+	fmt.Println("データベース接続成功")
+
+	return db
+}
