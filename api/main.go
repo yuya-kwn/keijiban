cdb891e82d1a340061d7164502831eaff52a574a
川野勇也 <kawanoisamunari@kawanoisamunarinoMacBook-Pro.local>
2023-02-10 00:06:28 +0900

    環境構築やり直した

--- api/main.go
+++ api/main.go
@@ -1,35 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"time"
+	"net/http"
 
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	"github.com/gin-gonic/gin"
 )
 
-func dbConnect() *gorm.DB {
-
-	dsn := "user:pass@tcp(db:3306)/keijiban?charset=utf8mb4"
-	count := 0
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		for {
-			if err == nil {
-				break
-			}
-			fmt.Println("データベース再接続...")
-			time.Sleep(time.Second)
-			count++
-			if count > 60 {
-				fmt.Println("データベース接続失敗")
-				log.Fatal(err)
-			}
-			db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-		}
-	}
-	fmt.Println("データベース接続成功")
-
-	return db
+func main() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello,World!!",
+		})
+	})
+	router.Run(":3030")
 }
