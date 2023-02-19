0550802820b48061dc2756019dffbba0ee7719e9
川野勇也 <kawanoisamunari@kawanoisamunarinoMacBook-Pro.local>
2023-02-19 15:38:12 +0900

    htmlレンダリング

--- controller/router.go
+++ controller/router.go
@@ -1,8 +1,16 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func GetRouter() *gin.Engine {
 	r := gin.Default()
+	r.LoadHTMLGlob("view/index.html")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
 	return r
 }
